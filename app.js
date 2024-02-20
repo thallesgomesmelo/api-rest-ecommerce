f8c34b6d03841b72c7556a1e537c30d204a6889e
thalles.melo <thalles.melo@mestria.com.br>
2024-02-20 10:10:45 -0300

    Feito funções que trata erros que pode ser causado nas rotas ou por tentativa de acesso a rotas inexistentes.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
 
 const app = express(); // Intancia
 
+// Retorna logs durante o momento de desenvolvimento.
+app.use(morgan("dev"));
+
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
+
+/**
+ * Quando passar por todas as rotas acima e
+ * não entrar em nunhuma vai ser usado este metodo.
+ */
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+
+  erro.status = 404;
+
+  next(erro);
+});
+
+// Utilizado para quando ocorrer algum erro em alguma rota a cima.
+app.use((erro, req, res, next) => {
+  res.status(erro.status || 500);
+  return res.json({
+    erro: {
+      message: erro.message
+    }
+  });
+});
 
 module.exports = app;
