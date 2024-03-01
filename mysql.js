2a6894f9aed92c7e9fc7a4c7c4be10747da80b02
thalles.melo <thalles.melo@mestria.com.br>
2024-03-01 13:44:19 -0300

    Refatorado função de conexão com o banco de dados.

--- mysql.js
+++ mysql.js
@@ -5,7 +5,20 @@ const poll = mysql.createPool({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT
+  port: process.env.MYSQL_PORT,
+  connectionLimit: 1000
 });
+
+exports.execute = (query, params = []) => {
+  return new Promise((resolve, reject) => {
+    poll.query(query, params, (erro, result, fields) => {
+      if (erro) {
+        reject(erro);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
 
 exports.poll = poll;
